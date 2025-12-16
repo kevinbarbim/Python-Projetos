575ccdeb29bc8eef3ae4a75dca5cc4362507174d
Kevin Barbim <kevinebdecastilho@gmail.com>
2025-12-16 20:22:05 -0300

    Delete alurapython/Praticando Python - Trabalhando com projetos directory

--- alurapython/Praticando Python - Trabalhando com projetos/gorjeta/gorjeta.py
+++
@@ -1,9 +0,0 @@
-def main():
-  conta = float(input("Digite sua conta: "))
-  gorjeta = float(input("Digite a % de gorjeta: "))
-  resultado = calculo(conta,gorjeta)
-  print(f"Resultado: {resultado}")  
-def calculo(conta,gorjeta):
-  num = conta + conta*(gorjeta/100)
-  return num
-main()
